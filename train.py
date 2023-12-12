e7ab80b16accb02ba46bd84b0ce915ccc6a6b4e2
Bartosz Żuk <bartosz.zuk@aiqlabs.pl>
2023-12-12 17:52:11 +0100

    Added command line arguments

--- train.py
+++ train.py
@@ -1,11 +1,13 @@
-import os
+import argparse
 import warnings
 from dataclasses import dataclass
+from pathlib import Path
 
 import torch
 from lightning import Trainer
 from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
 from lightning.pytorch.loggers import TensorBoardLogger
+from typing_extensions import Self
 
 from data.dataset import CIFAR10DataModule
 from model import models
@@ -14,27 +16,46 @@ from model.modules import SelfSupervisedModel
 torch.set_float32_matmul_precision('medium')
 
 
+@dataclass(frozen=True)
+class TrainArguments:
+    dataset: Path | str = 'cifar10'
+    batch_size: int = 1024
+    num_views: int = 16
+    max_epochs: int = 500
+    learning_rate: float = 1e-3
+    projection_dim: int = 128
+    num_neighbours: int = 20
+    dev: bool = False
+
+    @classmethod
+    def from_command_line(cls, arguments: argparse.Namespace) -> Self:
+        return cls(**vars(arguments))
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=Path, default='cifar10')
+    parser.add_argument('--batch-size', type=int, default=1024)
+    parser.add_argument('--num-views', type=int, default=16)
+    parser.add_argument('--max-epochs', type=int, default=500)
+    parser.add_argument('--learning-rate', type=int, default=1e-3)
+    parser.add_argument('--projection-dim', type=int, default=128)
+    parser.add_argument('--num-neighbours', type=int, default=20)
+    parser.add_argument('--dev', action='store_true')
+
+    return parser.parse_args()
+
+
 def silence_deprecation_warnings() -> None:
     for line in [110, 111, 117, 118]:
         warnings.filterwarnings('ignore', category=UserWarning, module='torch.overrides', lineno=line)
 
 
-@dataclass(frozen=True)
-class TrainArguments:
-    batch_size: int = 1024
-    num_views: int = 16
-    num_workers: int = os.cpu_count() - 2
-    max_epochs: int = 500
-    learning_rate: float = 1e-3
-    projection_dim: int = 128
-    num_neighbours: int = 20
-
-
 def train():
     silence_deprecation_warnings()
 
-    args = TrainArguments()
-    data = CIFAR10DataModule('cifar10', args.num_views, args.batch_size, dev=True)
+    args = TrainArguments.from_command_line(parse_arguments())
+    data = CIFAR10DataModule('cifar10', args.num_views, args.batch_size, args.dev)
 
     knn = models.KNearestNeighbours(args.num_neighbours)
 
